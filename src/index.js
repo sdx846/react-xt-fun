0d95651e8d0f2e9ad03530dc25a57237cea4f315
sdx846 <sdx_liuhui@163.com>
2021-03-03 11:18:52 +0800

    路由更改

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import Router from './router';
+import Router from './router';
 import reportWebVitals from "./reportWebVitals";
-import {
-  HashRouter as Routers,
-  Switch,
-  Redirect,
-  Route,
-} from "react-router-dom";
-import Home from "./pages/home/home";
-import Login from "./pages/login/login";
-import PageNotFound from "./pages/pageOther/pageNotFound";
-import { localStore } from "./utils/storage";
 
 ReactDOM.render(
-  <Routers>
-    <Switch>
-      <Route
-        path="/home"
-        render={(props) =>localStore.getItem("token") ? (<Home {...props} />) : (<Redirect to="/login" />)}
-      />
-      <Route path="/login" component={Login} />
-      <Redirect exact from="/" to="/home/system/role" />
-      <Route path="*" component={PageNotFound} />
-    </Switch>
-  </Routers>,
+  <Router/>,
   document.getElementById("root")
 );
 
